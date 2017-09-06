feee5d6c34c4d59799d96264fe003f2426d6465f
Ian Brown <swish1zero1@gmail.com>
2017-09-06 17:21:47 -0400

    print prices and 2x, 3x, 10x

--- program.py
+++ program.py
@@ -31,24 +31,58 @@ def average_opening_price(number_days, df):
 
 
 def opening_price(number_days, df):
-    df = df[df.loc[:, 'Market Cap'] != '-']  # Remove Null Market Caps
-    return df.iloc[-number_days, :]['Open'].mean()
+    return df.iloc[-number_days, :]['Open']
 
 
 def print_alive_prices(coin, days_alive, number_days, df):
+    interval_list = []
+    aop = average_opening_price(number_days, df)
     print('{} has been alive for {} days.'.format(coin, days_alive))
     print('First {} days average opening price: '.format(number_days) +
-          '{}'.format(average_opening_price(number_days, df)))
-    for i in range(30, days_alive, 30):
-        print('Price at {} days: {}'.format(i, opening_price(i, df)))
+          '{}'.format(aop))
+    for i in range(1, days_alive - 1):
+        # print('Price at {} days: {}'.format(i, opening_price(i, df)))
+        interval_list.append(opening_price(i, df))
+    return aop, interval_list
 
 
+def check_price_decrease(aop_list_tuple):
+    temp_int = 0
+    start_date = 30
+    for i in range(start_date, len(aop_list_tuple[1])):
+        if aop_list_tuple[0] < aop_list_tuple[1][i]:
+            print('Day {}: {} the price was higher than ICO: {}'.format(i, aop_list_tuple[1][i], aop_list_tuple[0]))
+            temp_int = i
+            break
+    for i in range(temp_int, len(aop_list_tuple[1])):
+        if (2*aop_list_tuple[0]) < aop_list_tuple[1][i]:
+            print('Day {}: {} the price was more than double the ICO'.format(i, aop_list_tuple[1][i]))
+            temp_int = i
+            break
+    for i in range(temp_int, len(aop_list_tuple[1])):
+        if (3 * aop_list_tuple[0]) < aop_list_tuple[1][i]:
+            print('Day {}: {} the price was more than triple the ICO'.format(i, aop_list_tuple[1][i]))
+            temp_int = i
+            break
+    for i in range(temp_int, len(aop_list_tuple[1])):
+        if (10 * aop_list_tuple[0]) < aop_list_tuple[1][i]:
+            print('Day {}: {} the price was more than ten times the ICO'.format(i, aop_list_tuple[1][i]))
+            temp_int = i
+            break
+
+
+
+# the price decreased
+# it took such and such days for it to go above ico price
+#
 def main():
-    coin = 'ethereum'
+    coin = 'waves'
     df = get_historical_data(coin)
     days_alive = df.shape[0]
     number_days = 3
-    print_alive_prices(coin, days_alive, number_days, df)
+    aop_list_tuple = print_alive_prices(coin, days_alive, number_days, df)
+    check_price_decrease(aop_list_tuple)
+
 
 if __name__ == '__main__':
     main()
