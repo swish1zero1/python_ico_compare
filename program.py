72ae84b94f13a1ff66a84edab8812213bfc709cb
Ian Brown <swish1zero1@gmail.com>
2017-09-06 14:13:38 -0400

    print average opening price

--- program.py
+++ program.py
@@ -1,11 +1,9 @@
-import json
-from pprint import pprint
 import requests
+import pandas as pd
+import datetime
 
 
-def main():
-    coin = 'ethereum'
-
+def print_coin_or_coins(coin):
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/')
 
     if coin is not '':
@@ -19,6 +17,23 @@ def main():
         for line in r.json():
             print(line)
 
+
+def get_historical_data(coin):
+    url = 'https://coinmarketcap.com/currencies/{}/historical-data/?start=19760101&end=21000101'.format(coin)
+    test_pandas = pd.read_html(url)
+    df = test_pandas[0]
+    return df
+
+
+def average_opening_price(number_first_days, coin):
+    df = get_historical_data(coin)
+    return df.iloc[-number_first_days:, :]['Open'].mean()
+
+
+def main():
+    coin = 'ethereum'
+    number_first_days = 3
+    print(average_opening_price(number_first_days, coin))
 
 if __name__ == '__main__':
     main()
