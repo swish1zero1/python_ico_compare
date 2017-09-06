fc79ecd8cc880059763cc4c14decb836d9b37422
Ian Brown <swish1zero1@gmail.com>
2017-09-06 13:09:55 -0400

    print one coin or all coins

--- program.py
+++ program.py
@@ -4,9 +4,20 @@ import requests
 
 
 def main():
+    coin = 'ethereum'
+
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/')
-    for line in r.json():
-        print(line['id'])
+
+    if coin is not '':
+        print('Searching coin {}'.format(coin))
+        for line in r.json():
+            if line['id'] == coin:
+                print(line)
+                break  # Once found, stop the loop
+    else:
+        print('Coin field is blank. Printing all coins:')
+        for line in r.json():
+            print(line)
 
 
 if __name__ == '__main__':
